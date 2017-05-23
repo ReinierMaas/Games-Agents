e1854fd879e10130ccfa311f52c36babdcb60b9d
Reinier Maas <reiniermaas@hotmail.com>
2017-05-23 11:24:57 +0200

    start 4 clients script

---
+++ start.bat
@@ -0,0 +1,4 @@
+cd "C:\Malmo-0.21.0-Windows-64bit\Minecraft\" && start launchClient
+cd "C:\Malmo-0.21.0-Windows-64bit\Minecraft\" && start launchClient
+cd "C:\Malmo-0.21.0-Windows-64bit\Minecraft\" && start launchClient
+cd "C:\Malmo-0.21.0-Windows-64bit\Minecraft\" && start launchClient
