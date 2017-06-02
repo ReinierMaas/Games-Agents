e0e491ea3a5e0dc080ea2e8c216aae1e84bbd5f5
Douwe van Gijn <kipstad@hotmail.com>
2017-06-02 14:30:19 +0200

    wip, this is the interface I'm aiming for: integer state ipv boolean state

--- goap.py
+++ goap.py
@@ -1,26 +1,25 @@
 from goapy import Planner, Action_List
 
-def chopWood(arg):
+def chopWood(w):
     print 'chopping wood! chop chop...'
 
-def craftTable(arg):
-    print 'crafting crafting table! table... in the color %s' % arg
+def craftTable(w):
+    print 'crafting crafting table! table...'
 
-def craftPlank(arg):
+def craftPlank(w):
     print 'crafting planks! plank plank...'
-
 
 if __name__ == '__main__':
     import time
-    world = Planner('has_4_planks', 'has_crafting_table', 'has_logs')
-    world.set_start_state(has_4_planks=False, has_logs=False, has_crafting_table=False)
-    world.set_goal_state(has_crafting_table=True)
+    world = Planner('planks', 'crafting_tables', 'logs')
+    world.set_start_state(planks=0, crafting_tables=0, logs=0)
+    world.set_goal_state(crafting_tables=1)
 
     actions = Action_List()
 
-    actions.add_action('craft_table', craftTable, {'has_4_planks':True}, {'has_crafting_table':True})
-    actions.add_action('craft_planks', craftPlank, {'has_logs':True}, {'has_4_planks':True})
-    actions.add_action('chop_wood', chopWood, {}, {'has_logs':True})
+    actions.add_action('craft_table',  craftTable, {'planks':4}, {'crafting_tables':1, 'planks':-4})
+    actions.add_action('craft_planks', craftPlank, {'logs':1},   {'planks':4, 'logs':-1})
+    actions.add_action('chop_wood',    chopWood,   {},           {'logs':1})
 
     world.set_action_list(actions)
 
@@ -33,4 +32,16 @@ if __name__ == '__main__':
         print path.index(p)+1, p['name']
 
     print '\nTook:', took_time
+
+# future idea to make defining actions more flexible by using functions
+# may be overkill, but would definitly be cool
+class CraftTable:
+    def priority(w):
+        return 1
+    def requires(w): 
+        return w['planks']>=4
+    def changes(w):
+        return {'crafting_tables':1,'planks':-4}
+    def does(arg):
+        print 'crafting tables!'
 
